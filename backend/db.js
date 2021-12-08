3445bcce7c79f9acda37056f04d3b02c5f86202d
UTKARSH TRIPATHI <utkarsh.ilh@gmail.com>
2021-12-09 01:04:42 +0530

    ...

---
+++ backend/db.js
@@ -0,0 +1,9 @@
+const mangoose = require('mongoose');
+const mongoURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false"
+
+const connectToMongo = () => {
+    mangoose.connect(mongoURI, () => {
+        console.log("connected sucessfully");
+    })
+}
+module.exports = connectToMongo;
