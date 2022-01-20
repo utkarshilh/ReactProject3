7a86e4131077dadffa76ca4aadc219f997d0ec93
UTKARSH TRIPATHI <utkarsh.ilh@gmail.com>
2022-01-20 22:52:19 +0530

    20th janyary 2022

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { Mongoose } = require('mongoose');
-// const User = require('../models/User')
+const User = require('../models/User')
 const router = express.Router();
 
 
